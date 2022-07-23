0df6a669f95b3837a26952add9104ed0f5b9c1c3
Taras Mavdryk <mavdruktaras@gmail.com>
2022-07-23 12:25:13 +0300

    initial commit

---
+++ djackets_django/product/models.py
@@ -0,0 +1,68 @@
+from io import BytesIO
+from tkinter import CASCADE
+from PIL import Image
+
+from django.core.files import File
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    
+    class Meta:
+        ordering = ('name',)
+        
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='name', on_delete=CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ('date_added')
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.category}/{self.slug}/'
+    
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''
+    
+    def make_thumbnail(self, image, size=(300,300)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+        
+        thumbnail_io = BytesIO
+        img.save(thumbnail_io, 'JPEG', quality=85)
+        
+        thumbnail = File(thumbnail_io, name=image.name)
+        
+        return thumbnail  
+    
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return 'http://127.0.0.1:8000' + self.image.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+                
+                return 'http://127.0.0.1:8000' + self.image.url
+            else:
+                return ''
