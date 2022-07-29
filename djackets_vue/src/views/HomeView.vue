4367ee57b7f5ddf8f0ec165463eba86539b591fd
Taras Mavdryk <mavdruktaras@gmail.com>
2022-07-30 01:35:20 +0300

    app Product.vue added

--- djackets_vue/src/views/HomeView.vue
+++ djackets_vue/src/views/HomeView.vue
@@ -1,33 +1,29 @@
 <template>
   <div class="home">
     <section class="hero is-medium is-dark mb-6">
-        <div class="hero-body has-text-centered">
-          <p class="title mb-6">
-            Welcome to Djackets
-          </p>
-          <p class="subtitle">
-            Best jacket store
-          </p>
-        </div>
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Djackets
+        </p>
+        <p class="subtitle">
+          Best jacket store
+        </p>
+      </div>
     </section>
 
     <div class="columns is-multiline">
       <div class="column is-12">
-          <h2 class="is-size-2 has-text-centered">Latest products</h2>
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
-      <div 
-      class="column is-3"
-      v-for="product in latestProducts"
-      v-bind:key="product.id"
-      >
+      <div class="column is-3" v-for="product in latestProducts" v-bind:key="product.id">
         <div class="box">
           <figure class="image mb-4">
             <img :src="product.get_thumbnail">
           </figure>
           <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{product.price}}</p>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
 
-          View details
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View deatails</router-link>
         </div>
       </div>
     </div>
@@ -46,28 +42,28 @@ export default {
   },
   components: {
   },
-    mounted() {
-      this.getLatestProducts()
+  mounted() {
+    this.getLatestProducts()
   },
   methods: {
     getLatestProducts() {
       axios
-      .get('api/v1/latest-products')
-      .then(response => {
-        this.latestProducts = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
+        .get('api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 }
 </script>
 
 <style scoped>
-  .image {
-      margin-top: -1.25rem;
-      margin-left: -1.25rem;
-      margin-right: -1.25rem;
-  }
+.image {
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
 </style>
